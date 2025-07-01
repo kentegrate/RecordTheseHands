d4a88baaf2105ec4ef6861e246bcd1a6f5df9f9e
Ken Takaki <kenkiki94305@gmail.com>
2025-07-01 10:58:41 +0900

    Changed to high resolution of pixel tablets (not tested enough)

--- app/src/main/java/edu/gatech/ccg/recordthesehands/recording/RecordingActivity.kt
+++ app/src/main/java/edu/gatech/ccg/recordthesehands/recording/RecordingActivity.kt
@@ -270,7 +270,7 @@ class RecordingActivity : AppCompatActivity() {
      * Record video at 15 Mbps. At 1944x2592 @ 30 fps, this level of detail should be more
      * than high enough.
      */
-    private const val RECORDER_VIDEO_BITRATE: Int = 15_000_000
+    private const val RECORDER_VIDEO_BITRATE: Int = 30_000_000
 
     /**
      * Height, width, and frame rate of the video recording. Using a 4:3 aspect ratio allows us
@@ -1431,6 +1431,7 @@ class RecordingActivity : AppCompatActivity() {
     val props = cameraManager.getCameraCharacteristics(cameraId)
 
     val sizes = props.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+    Log.i(TAG, "Supported video resolutions: ${sizes?.getOutputSizes(MediaRecorder::class.java)?.contentToString()}")
     Log.i(TAG, sizes.toString())
 
     fun hasAspectRatio(heightRatio: Int, widthRatio: Int, dim: Size): Boolean {
@@ -1452,7 +1453,7 @@ class RecordingActivity : AppCompatActivity() {
           && (hasAspectRatio(heightRatio, widthRatio, it))
     }?.maxByOrNull { it.width * it.height }
 
-    val chosenSize = largestAvailableSize ?: Size(RECORDING_HEIGHT, RECORDING_WIDTH)
+    val chosenSize = Size(3264, 2448)
 
 
     Log.i(TAG, "Selected video resolution: ${chosenSize.width} x ${chosenSize.height}")
